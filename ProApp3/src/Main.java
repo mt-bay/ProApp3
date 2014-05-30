0bbd34ffc586781db521bd3ec52bbbc1a86020cf
s10532 <s10532@122PC39.salesio.salesio-sp.ac.jp>
2014-05-30 14:28:42 +0900

    Slick2Dわからんぞえ

--- ProApp3/src/Main.java
+++
@@ -1,9 +0,0 @@
-.
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
-}
---
+++ ProApp3/src/window/window.java
@@ -0,0 +1,12 @@
+/*
+ * windowクラス
+ * ウィンドウの状態などを定義・保存する
+ */
+package window;
+
+
+public class window {
+	public window(){
+		
+	}
+}
