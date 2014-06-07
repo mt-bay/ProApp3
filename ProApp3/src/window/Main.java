a5b036df958a83afd53b640ef64afc0f008cd977
mt-bay <mountainbay89@gmail.com>
2014-06-08 01:09:01 +0900

    Mainクラス整備， point，lineクラス作成， polygonalクラス(折れ線)仮作成

---
+++ ProApp3/src/window/Main.java
@@ -0,0 +1,60 @@
+/*
+ * mainクラス
+ * プログラムの起動・終了
+ */
+
+package window;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+public class Main extends BasicGame{
+
+	public Main(String title) {
+		super(title);
+	}
+
+
+	public static void main(String[] args) throws SlickException{
+
+	}
+
+
+	/* 初期化
+	 * 目的  ：ファイルを読み込んで変数と関連付ける
+	 * 戻り値：void
+	 * 例外  ：SlickException
+	 */
+	@Override
+	public void init(GameContainer gc) throws SlickException{
+		//初期化ルーチン
+
+	}
+
+
+	/* 内容の更新
+	 * 目的  ：内部状態の更新(メインループ)
+	 * 戻り値：void
+	 * 例外  ：SlickException
+	 */
+	@SuppressWarnings("static-access")
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException{
+
+	}
+
+	/* 描画
+	 * 目的  ：画面描画(メインループ)
+	 * 戻り値：void
+	 * 例外  ：SlickException
+	 */
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException{
+
+
+	}
+
+}
