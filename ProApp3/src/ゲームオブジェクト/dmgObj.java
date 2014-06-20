a00295c59b1fcc055851682344b568cc3c1ed623
s10532 <s10532@122PC40.salesio.salesio-sp.ac.jp>
2014-06-20 14:08:50 +0900

    dmgObjコンストラクタ作成 ちょっとの更新でコミットしすぎな気がしてきた

--- ProApp3/src/ゲームオブジェクト/dmgObj.java
+++ ProApp3/src/ゲームオブジェクト/dmgObj.java
@@ -12,11 +12,26 @@ public class dmgObj extends rect {
 	 * 引数：なし
 	 */
 	public dmgObj(){
-		super();
+		set(new point(), new point(), new point(), 0.0);
+	}
+	/* コピーコンストラクタ
+	 * 引数：コピー元
+	 */
+	public dmgObj(dmgObj obj){
+		set(obj.location, obj.size, obj.accel, obj.atk);
+	}
+	/* 値指定型コンストラクタ
+	 * 引数：それぞれのデータ
+	 */
+	public dmgObj(point Loc, point Siz, point Acc, double Atk){
+		set(Loc, Siz, Acc, Atk);
 	}
 	
 	
 	/* メソッド */
+	/* setter
+	 * 引数：それぞれのデータ
+	 */
 	private void set(point Loc, point Siz, point Acc, double Atk){
 		location = new point(Loc);
 		size     = new point(Siz);
