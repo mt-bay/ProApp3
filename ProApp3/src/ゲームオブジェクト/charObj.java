7b193fc47a5fe74fce9cbb5af874abb0c565b6e0
s10532 <s10532@122PC40.salesio.salesio-sp.ac.jp>
2014-06-20 13:56:59 +0900

    アルプスカードマン

--- ProApp3/src/ゲームオブジェクト/charObj.java
+++ ProApp3/src/ゲームオブジェクト/charObj.java
@@ -1,7 +1,9 @@
 package ゲームオブジェクト;
 
+import 座標系.point;
 import 座標系.rect;
 
 public class charObj extends rect {
+	point accel;	//移動力
 	
 }
--- ProApp3/src/ゲームオブジェクト/dmgObj.java
+++ ProApp3/src/ゲームオブジェクト/dmgObj.java
@@ -1,7 +1,9 @@
 package ゲームオブジェクト;
 
+import 座標系.point;
 import 座標系.rect;
 
 public class dmgObj extends rect {
-	
+	point  accel;	//移動力
+	double atk;		//charObj衝突時の体力変動値
 }
