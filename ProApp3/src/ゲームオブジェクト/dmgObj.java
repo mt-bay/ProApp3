6e9a81b42f3370d6292eff98739fd02f359702c4
s10532 <s10532@122PC40.salesio.salesio-sp.ac.jp>
2014-06-20 14:03:02 +0900

    むやみにgetter, setter作るのはやめよう!

--- ProApp3/src/ゲームオブジェクト/dmgObj.java
+++ ProApp3/src/ゲームオブジェクト/dmgObj.java
@@ -4,6 +4,23 @@ import 座標系.point;
 import 座標系.rect;
 
 public class dmgObj extends rect {
-	point  accel;	//移動力
-	double atk;		//charObj衝突時の体力変動値
+	public point  accel;	//移動力
+	public double atk;		//charObj衝突時の体力変動値
+	
+	/* コンストラクタ */
+	/* デフォルトコンストラクタ
+	 * 引数：なし
+	 */
+	public dmgObj(){
+		super();
+	}
+	
+	
+	/* メソッド */
+	private void set(point Loc, point Siz, point Acc, double Atk){
+		location = new point(Loc);
+		size     = new point(Siz);
+		accel    = new point(Acc);
+		atk      = Atk;
+	}
 }
