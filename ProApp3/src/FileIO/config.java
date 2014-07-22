f8eb66dd6e89ca03c868b675f5cef4ffb1dc7e23
mt-bay <mountainbay89@gmail.com>
2014-07-22 14:30:27 +0900

    config.javaの整備中…

--- ProApp3/src/FileIO/config.java
+++ ProApp3/src/FileIO/config.java
@@ -1,9 +1,9 @@
 package FileIO;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 import org.newdawn.slick.Input;
 
@@ -13,10 +13,11 @@ import org.newdawn.slick.Input;
 public class config {
 	/* メンバ変数 */
 	//ファイル関係
+	private static String     fName  = "config.cfg";
 	private static FileWriter fWrite;
-	private static String     fName;
 	
 	//ボタン関係
+	public  static final int useBtn = 8;	//使用するボタン数
 	public  static int       attack;
 	public  static int       jump;
 	public  static int       left;
@@ -25,31 +26,24 @@ public class config {
 	public  static int       right;
 	public  static int       ok;
 	public  static int       cancel;
-	/* コンストラクタ */
-	
-	/* デフォルトコンストラクタ
-	 * fileからデータを読み込む(存在しない場合はfileにあたらしくデータを追加)
-	 */
-	public config(){
-	}
-	
 	
 	/* メソッド */
 	/* 初期化
 	 * 引数：なし 
 	 */
-	public void init(){
+	static{
 		try{
 			//ファイル読み込み
-			BufferedReader fRead = new BufferedReader(new FileReader(fName));
+			readBtn();
 			
 			
-		}catch(FileNotFoundException e){
+		}catch(IOException e){
 			//ファイル読み込みに失敗したら、コンフィグファイルを作成
 			try{
 				
-				FileWriter fWrite = new FileWriter(fName); 
-				buttonSet(Input.KEY_Z, Input.KEY_X, Input.KEY_LEFT, Input.KEY_DOWN, Input.KEY_UP, Input.KEY_RIGHT, Input.KEY_Z, Input.KEY_X);
+				FileWriter fWrite = new FileWriter(fName);
+				setBtn(Input.KEY_Z, Input.KEY_X, Input.KEY_LEFT, Input.KEY_DOWN, Input.KEY_UP, Input.KEY_RIGHT, Input.KEY_Z, Input.KEY_X);
+				
 				
 			}catch(Exception e1){
 				//未実装
@@ -59,17 +53,54 @@ public class config {
 	}
 	
 	/* 使用ボタンセット
-	 * 
+	 * 引数：それぞれのデータ
 	 */
-	public void buttonSet(int _attack, int _jump,
-			              int _left, int _down, int _up, int _right,
-			              int _ok, int _cancel){
+	public  static void setBtn(int _attack, int _jump  ,
+			                   int _left  , int _down  , int _up, int _right,
+			                   int _ok    , int _cancel){
 		
 		attack = _attack;
 		jump   = _jump;
 		left   = _left;
+		down   = _down;
+		up     = _up;
 		right  = _right;
 		ok     = _ok;
 		cancel = _cancel;
 	}
+	
+	/* 使用ボタンファイル書き込み
+	 * 引数：
+	 */
+	public  static void writeBtn(){
+		
+	}
+	
+	/* 使用ボタンファイル読み込み
+	 * 引数：なし
+	 */
+	private static void readBtn() throws IOException{
+		BufferedReader bRead = new BufferedReader(new FileReader(fName));
+		String         fst   = bRead.readLine();
+		String[]       sBuf  = new String[8];
+		
+		//データ分割
+		for(int i = 0; i < useBtn; i++)
+			sBuf[i] = fst.substring(i * 8, (i + 1) * 8);
+		
+		//データ挿入
+		attack = Integer.parseInt(sBuf[0], 16);
+		jump   = Integer.parseInt(sBuf[1], 16);
+		left   = Integer.parseInt(sBuf[2], 16);
+		down   = Integer.parseInt(sBuf[3], 16);
+		up     = Integer.parseInt(sBuf[4], 16);
+		right  = Integer.parseInt(sBuf[5], 16);
+		ok     = Integer.parseInt(sBuf[6], 16);
+		cancel = Integer.parseInt(sBuf[7], 16);
+		
+		bRead.close();
+		return;
+	}
+	
+	
 }
