e2364a1752b671f46f1b4251cf4c48a5cef81e6e
mt-bay <mountainbay89@gmail.com>
2014-07-22 21:49:34 +0900

    config.java整備

--- ProApp3/src/FileIO/config.java
+++ ProApp3/src/FileIO/config.java
@@ -1,6 +1,8 @@
 package FileIO;
 
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,51 +16,48 @@ public class config {
 	/* メンバ変数 */
 	//ファイル関係
 	private static String     fName  = "config.cfg";
-	private static FileWriter fWrite;
-	
+
 	//ボタン関係
 	public  static final int useBtn = 8;	//使用するボタン数
-	public  static int       attack;
-	public  static int       jump;
-	public  static int       left;
-	public  static int       down;
-	public  static int       up;
-	public  static int       right;
-	public  static int       ok;
-	public  static int       cancel;
-	
+	public  static       int attack;
+	public  static       int jump;
+	public  static       int left;
+	public  static       int down;
+	public  static       int up;
+	public  static       int right;
+	public  static       int ok;
+	public  static       int cancel;
+
 	/* メソッド */
 	/* 初期化
-	 * 引数：なし 
+	 * 引数：なし
 	 */
 	static{
 		try{
-			//ファイル読み込み
+			//ファイル読み込み, ボタン割り当て
 			readBtn();
-			
-			
+
 		}catch(IOException e){
 			//ファイル読み込みに失敗したら、コンフィグファイルを作成
 			try{
-				
-				FileWriter fWrite = new FileWriter(fName);
+
 				setBtn(Input.KEY_Z, Input.KEY_X, Input.KEY_LEFT, Input.KEY_DOWN, Input.KEY_UP, Input.KEY_RIGHT, Input.KEY_Z, Input.KEY_X);
-				
-				
+				writeBtn();
+
 			}catch(Exception e1){
 				//未実装
 				System.exit(1);
 			}
 		}
 	}
-	
+
 	/* 使用ボタンセット
 	 * 引数：それぞれのデータ
 	 */
 	public  static void setBtn(int _attack, int _jump  ,
-			                   int _left  , int _down  , int _up, int _right,
-			                   int _ok    , int _cancel){
-		
+	                           int _left  , int _down  , int _up, int _right,
+	                           int _ok    , int _cancel){
+
 		attack = _attack;
 		jump   = _jump;
 		left   = _left;
@@ -68,26 +67,41 @@ public class config {
 		ok     = _ok;
 		cancel = _cancel;
 	}
-	
+
 	/* 使用ボタンファイル書き込み
-	 * 引数：
+	 * 引数：なし
 	 */
-	public  static void writeBtn(){
-		
+	public  static void writeBtn() throws IOException{
+		BufferedWriter bWrite = new BufferedWriter(new FileWriter(fName));
+		String         sWrite = String.format("%8X", attack) +
+				                String.format("%8X", jump  ) +
+				                String.format("%8X", left  ) +
+				                String.format("%8X", down  ) +
+				                String.format("%8X", up    ) +
+				                String.format("%8X", right ) +
+				                String.format("%8X", ok    ) +
+				                String.format("%8X", cancel) ;
+
+		bWrite.write(sWrite);
+		bWrite.close();
+
+		return;
 	}
-	
+
 	/* 使用ボタンファイル読み込み
 	 * 引数：なし
 	 */
 	private static void readBtn() throws IOException{
 		BufferedReader bRead = new BufferedReader(new FileReader(fName));
 		String         fst   = bRead.readLine();
-		String[]       sBuf  = new String[8];
-		
-		//データ分割
-		for(int i = 0; i < useBtn; i++)
+		String[]       sBuf  = new String[useBtn];
+
+		bRead.close();
+
+		for(int i = 0; i < sBuf.length; i++)
 			sBuf[i] = fst.substring(i * 8, (i + 1) * 8);
-		
+
+
 		//データ挿入
 		attack = Integer.parseInt(sBuf[0], 16);
 		jump   = Integer.parseInt(sBuf[1], 16);
@@ -97,10 +111,9 @@ public class config {
 		right  = Integer.parseInt(sBuf[5], 16);
 		ok     = Integer.parseInt(sBuf[6], 16);
 		cancel = Integer.parseInt(sBuf[7], 16);
-		
-		bRead.close();
+
 		return;
 	}
-	
-	
+
+
 }
