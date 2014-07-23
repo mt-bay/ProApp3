b128eb8f50fa560b9eb5338df13a4a802c01c4d3
mt-bay <mountainbay89@gmail.com>
2014-07-23 16:40:22 +0900

    playerObj, charObj着手開始

--- ProApp3/src/gameObj/playerObj.java
+++ ProApp3/src/gameObj/playerObj.java
@@ -1,8 +1,19 @@
 package gameObj;
 
+import org.newdawn.slick.*;
+
+import IO.config;
+
 public class playerObj extends charObj{
 	/* メンバ変数 */
-	boolean isDrone;
+	//状態変数
+	public    boolean isShooting; //シューティングモードか
+	protected int     t_deform;   //シューティング←→アクションの変形残りフレーム
+
+	private   double  act_mv;    //アクションモード時の左右移動力
+
+
+	//ユーザ入力関係
 
 	/* コンストラクタ */
 
@@ -12,6 +23,20 @@ public class playerObj extends charObj{
 	 */
 	@Override
 	public void update(){
+		//変形中 or 変形終了時
+		if(t_deform >= 0){
+			if(t_deform == 0)
+				isShooting = !isShooting;
+			t_deform--;
+		}
+		//通常時
+		else
+		{
+			//アクションモード時
+			if(!isShooting){
+				if(Input.isKeyDown(config.left))
+			}
+		}
 
 	}
 
