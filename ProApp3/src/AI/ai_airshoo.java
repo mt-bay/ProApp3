c6c3c07d51a4d5b27218bdb974bfcc2f6643e833
S10529 <S10529@410-42.salesio.salesio-sp.ac.jp>
2014-10-10 10:37:10 +0900

    空中砲台暫定版 ・空中にひし形を描くような形で飛び回り、弾を斜め下方向に飛ばします

--- ProApp3/src/AI/ai_airshoo.java
+++ ProApp3/src/AI/ai_airshoo.java
@@ -20,6 +20,40 @@ public class ai_airshoo {
 		_code.move   = ai_op.MOVE_NONE;
 		_code.unique = ai_op.UNIQUE_NONE;
 
+		
+		//向く方向を変える
+		if((_code.time_move == ai_op.TIME_MIN && _belong.ai_prev.get(0).time_move != _code.time_move) ||
+				((_code.move & ai_op.MOVE_DIR) == ai_op.MOVE_NONE)){
+			if(_code.time_move == ai_op.TIME_MIN){
+				_code.time_move = 8;
+			}
+			if(_code.time_move >= 7){
+				_code.move += ai_op.MOVE_DIR_UP;
+				_code.move += ai_op.MOVE_DIR_RIGHT;
+				_code.attack += ai_op.ATTACK_NOMAL;
+			}
+			else if(_code.time_move >= 5){ 
+				_code.move += ai_op.MOVE_DIR_DOWN;
+				_code.move += ai_op.MOVE_DIR_RIGHT;
+				_code.attack += ai_op.ATTACK_NOMAL;
+			}
+			else if(_code.time_move >= 3){ 
+				_code.move += ai_op.MOVE_DIR_DOWN;
+				_code.move += ai_op.MOVE_DIR_LEFT;
+				_code.attack += ai_op.ATTACK_NOMAL;
+			}
+			else if(_code.time_move <= 2){ 
+				_code.move += ai_op.MOVE_DIR_UP;
+				_code.move += ai_op.MOVE_DIR_LEFT;
+				_code.attack += ai_op.ATTACK_NOMAL;
+			}
+		}
+		else{
+			_code.move += _belong.ai_prev.get(0).move & ai_op.MOVE_DIR;
+		}
+		//移動力の決定
+		_code.move += ai_op.MOVE_MOVE_NORMAL;
+		
 		//攻撃
 		/*
 		 * 攻撃の激しさが距離によって変化する
