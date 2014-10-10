5861df9dba10b645e1cfacb816b997a9c7cc849a
S10529 <S10529@410-42.salesio.salesio-sp.ac.jp>
2014-10-10 10:05:13 +0900

    bigisland暫定版 １左移動 ２左ジャンプ移動しつつ弾を撃つ ３左ジャンプ移動しつつ弾を撃つ ってパターンで動きます。

--- ProApp3/src/AI/ai_bigisland.java
+++ ProApp3/src/AI/ai_bigisland.java
@@ -19,22 +19,10 @@ public class ai_bigisland {
 		_code.attack = ai_op.ATTACK_NONE;
 		_code.move   = ai_op.MOVE_NONE;
 		_code.unique = ai_op.UNIQUE_NONE;
-		//移動方向の決定
-		/*
-		 * 左に移動
-		 */
-		if((_code.time_move == ai_op.TIME_MIN &&
-		   _belong.ai_prev.get(0).time_move != _code.time_move) ||
-		   ((_code.move & ai_op.MOVE_DIR) == ai_op.MOVE_NONE)){
-				_code.move += ai_op.MOVE_DIR_LEFT;
-		}
-		else{
-			_code.move += _belong.ai_prev.get(0).move & ai_op.MOVE_DIR;
-		}
+
 		
 		/*攻撃パターン*/
-		
-		//突進
+		//突進 →　ジャンプ移動　→　ジャンプショット
 		if((_code.time_move == ai_op.TIME_MIN &&
 				   _belong.ai_prev.get(0).time_move != _code.time_move) ||
 				   ((_code.move & ai_op.MOVE_DIR) == ai_op.MOVE_NONE)){
@@ -42,39 +30,22 @@ public class ai_bigisland {
 						_code.time_move = 45;
 					}
 					if(_code.time_move >= 30){
-						
+						_code.move += ai_op.MOVE_DIR_LEFT;
 					}
 					else if(_code.time_move >= 16){
-						
-					}
-					else if(_code.time_move <= 15){
-						
-					}
-				}
-				else{
-					_code.move += _belong.ai_prev.get(0).move & ai_op.MOVE_DIR;
-				}
-		
-		//ふみつけ
-		if((_code.time_move == ai_op.TIME_MIN &&
-				   _belong.ai_prev.get(0).time_move != _code.time_move) ||
-				   ((_code.move & ai_op.MOVE_DIR) == ai_op.MOVE_NONE)){
-					if(_code.time_move == ai_op.TIME_MIN){
-						_code.time_move = 11;
 						_code.move += ai_op.MOVE_DIR_LEFT;
 						_code.move += ai_op.MOVE_JUMP_NORMAL;
+						_code.attack += ai_op.ATTACK_NOMAL;
+					}
+					else if(_code.time_move <= 15){
+						_code.move += ai_op.MOVE_JUMP_NORMAL;
+						_code.move += ai_op.MOVE_DIR_LEFT;
+						_code.attack += ai_op.ATTACK_NOMAL;
 					}
 				}
 				else{
 					_code.move += _belong.ai_prev.get(0).move & ai_op.MOVE_DIR;
 				}
-		
-		//ショット
-		if(Math.abs(_belong.location.x - _belong.belong.player_data.location.x) < 130.0){
-			_code.attack += ai_op.ATTACK_NOMAL;
-			_code.move += ai_op.MOVE_JUMP_NORMAL;
-		}
-		
 		//移動力の決定
 		_code.move += ai_op.MOVE_MOVE_NORMAL;
 	}
